811c04d567a5a4f5c52003f5903263d2608f2108
toriiarigato <aurelie.conan@yahoo.fr>
2021-10-18 12:07:58 +0200

    crud user ok

---
+++ Modele/classes/UserMgr.class.php
@@ -0,0 +1,132 @@
+<?php
+class UserMgr {
+
+        /**
+         * Permet d'obtenir une liste complète des utilisateur
+         * @param le mode de récupération des données (Tableau associatif par défaut)
+         * @return array la liste des utilisateurs
+         */
+        public static function getListUser() {
+            // Requête : la liste des utilisateurs
+            $sql = 'SELECT * FROM utilisateur ORDER BY ID_USE ASC';
+            // echo $sql; // pour mise au point
+
+            // Etape 1 - Crée une connexion BDD
+            $connexionPDO = Bdtk::getConnexion();
+            //var_dump($connexion);
+
+            // Etape 2 - Lance la requête
+            $resPDOstt = $connexionPDO->query($sql);
+            //var_dump($resPDOstt);
+
+            // Etape 3 - Lit tout le curseur PDOStatement
+            $records = $resPDOstt->fetchAll();
+            //var_dump($records);
+
+            // Etape 4 - Ferme le curseur et la connexion
+            $resPDOstt->closeCursor(); // ferme le curseur
+            Bdtk::disconnect();     // ferme la connexion
+
+            // Etape 5 - Retourne le tableau
+            return $records;
+        }
+
+
+        /**
+     * Fonction Add 
+     * 
+     * @param object utilisateur 
+     * 
+     * @return void 
+     */
+    public static function addUser($user) {
+
+        $connexion=Bdtk::getConnexion();
+
+        $sql = 'INSERT INTO utilisateur ( NOM_USE, PRENOM_USE,MDP_USE ,EMAIL_USE,ID_ROLE,LIB_AVATAR,DATENAISS_USE ,ADR_USE, CP_USE,VILLE_USE,ID_USE_CREATE,ID_USE_UPD,DATE_VAL_COTIS,ID_USE_DEL) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+
+        $resultats = $connexion->prepare($sql);
+        $resultats->execute(array($user->nomUser,$user->prenomUser,$user->mdpUser,$user->emailUser,$user->idRole,$user->libAvatar,$user->dateNaisseUser,$user->adrUser,$user->cpUser,$user->villeUser,$user->idUserCreate,$user->iduserUpdate,$user->dateValCot,$user->idUserdel));
+        Bdtk ::disconnect();
+        $count = $resultats->rowCount();
+        if ($count ==0){
+            return "Parametres erronnés ou inconnus";
+
+        }else{
+            return "Ajout de ".$count." lignes";
+        }
+    
+    }
+
+
+
+                /**
+         * Permet de supprimer l'utilisateur passé en paramètre
+         * @param un objet utilisateur
+         * @return nombre d'utilisateurs supprimés
+         */
+        public static function delUseryId($user) {
+            $sql = "DELETE FROM utilisateur WHERE ID_USE =?";
+            try {
+                $resultats = Bdtk::getConnexion()->prepare($sql);
+
+                // exécution requête
+
+                $resultats->execute(array($user));
+                
+                $nombre = $resultats->rowCount();
+                echo $nombre;
+                
+                // pour faire propre
+                $resultats->closeCursor();
+                Bdtk::disconnect();
+                $count = $resultats->rowCount();
+        if ($count ==0){
+            return "Parametres erronnés ou inconnus";
+
+        }else{
+            return "Suppression de ".$count." lignes";
+        }
+            }
+            catch(PDOException $e)
+            {
+            }
+            
+        }
+
+                        /**
+         * Permet de modifier l'utilisateur passé en paramètre
+         * @param un objet user
+         * @return nombre d'utilisateur mis à jour
+         */
+        public static function updateUser($user) {
+            // Prépare la requête SQL
+            $sql = "UPDATE utilisateur SET NOM_USE=?, PRENOM_USE=? WHERE ID_USE=?";
+            try {        
+            $resultats = Bdtk::getConnexion()->prepare($sql);
+
+            // exécution requête
+            $resultats->execute(array("Michel","Michel",$user)); // ATTENTION à l'ordre des attributs
+            
+            $nombre = $resultats->rowCount();
+            
+            // pour faire propre
+            $resultats->closeCursor();
+            Bdtk::disconnect();
+            $count = $resultats->rowCount();
+            if ($count ==0){
+                return "Parametres erronnés ou inconnus";
+
+            }else{
+                return "Modification de ".$count." lignes";
+            }
+        }
+            catch(PDOException $e)
+        {
+        
+        }
+        }
+
+}
+
+?>
--- controler/index.php
+++ controler/index.php
@@ -10,12 +10,12 @@
 <body>
     <h1>Connexion</h1>
     <!-- <a href="indextest.php">v1.1 connexion new PDO direct</a><br /> -->
-    <a href="indextest.php">v1.2 connexion via Bdtk et fichier de paramètres</a><br />
+    <a href="indextest.php">test crud</a><br />
     <a href="indexAuteur.php">Test des auteurs</a><br />
 
     <h1>Appel de requêtes SELECT</h1>
-    <!-- // <a href='index2.0.php'>v2.0 Liste des adherents en direct</a><br />
-    // <a href='index2.1.php'>v2.1 getListPilote()</a><br /> -->
+    <!-- <a href='index2.0.php'>v2.0 Liste des adherents en direct</a><br />
+    <a href='index2.1.php'>v2.1 getListPilote()</a><br /> -->
 
     <h1>Appel de requêtes préparées</h1>
     <!-- // <a href='index3.1.php'>v3.1 getPiloteById()</a><br />
@@ -23,6 +23,7 @@
     // <a href='index4.1.php'>v4.1 addPilote()</a><br />
     // <a href='index4.2.php'>v4.2 delPilote() et delPiloteById()</a><br />
     // <a href='index4.3.php'>v4.3 updatePilote()</a><br /> -->
+
 
 </body>
 
