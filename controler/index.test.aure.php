edbe64128038540f4b2a688ff6b55ac7747860df
Aurélie CONAN <21029219@GRN164.local>
2021-10-21 08:46:43 +0200

    relou

--- controler/index.test.aure.php
+++ controler/index.test.aure.php
@@ -19,6 +19,7 @@ $adresseUse = "";
 $libSerieDel = "";
 $codeEmpDel = "";
 $idSerieDel = "";
+$tSerie = [];
 
 $resultat = [];
 
--- views/view.header.php
+++ views/view.header.php
@@ -79,7 +79,7 @@
 
 
 
-    if ($action == 'bibli' or $action == 'gestionnaire' or $action == 'serie' or $action == 'addSerie' or $action == 'addSerieMaj' or $action == 'Supprimer Serie' or $action == "Modifier Serie" or $action == "modifMaj" or $action == 'responsable'or $action == 'emprunt'or $action == 'retour'or $action == 'nouvelAd'or $action == 'gestionAd'or $action == 'rechercheAd'or $action = "listSerie" ){ ?>
+    if ($action == 'bibli' or $action == 'gestionnaire' or $action == 'serie' or $action == 'addSerie' or $action == 'addSerieMaj' or $action == 'Supprimer Serie' or $action == "Modifier Serie" or $action == "modifMaj" or $action == 'responsable'or $action == 'emprunt'or $action == 'retour'or $action == 'nouvelAd'or $action == 'gestionAd'or $action == 'rechercheAd'or $action == "listSerie" ){ ?>
 
     <!-- Fonctionnne pour le GESTIONNAIRE DE FOND, LE RESPONSABLE ET LE BIBLIOTHECAIRE vu que les pages sont trés similaires -->
     <!--Grande Div qui englobe tout-->
